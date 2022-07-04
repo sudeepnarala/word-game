42396b11a608af65f50a624824738872ca900fa1
Sudeep Narala <sudeeprnarala@gmail.com>
2022-07-04 15:21:14 -0400

    Very partially working, without input

--- src/GamePage.jsx
+++ src/GamePage.jsx
@@ -1,47 +1,100 @@
-import {Button, Typography} from "@mui/material";
-import {db} from './word-game-firestore';
+import {Button, Grid, Typography} from "@mui/material";
+import {db, Pages} from './word-game-firestore';
 import { doc, setDoc, onSnapshot, query, collection } from "firebase/firestore";
 import {useState} from "react";
+// All english words
+import words from "an-array-of-english-words"
+import * as PropTypes from "prop-types";
+import {unstable_batchedUpdates} from "react-dom";
 
 
+let pdf = {"a": 4, "b": 2, "c": 2, "d": 2, "e": 3, "f": 2, "g": 2, "h": 3, "i": 3, "j": 2, "k": 2, "l": 2, "m": 2, "n": 2, "o": 2, "p": 2, "q": 1, "r": 2, "s": 3, "t": 3, "u": 4, "v": 2, "w": 2, "x": 1, "y": 2, "z": 1}
+
+let LETTERS = "abcdefghijklmnopqrstuvwxyz";
+
+// TODO: Host can end game
+// TODO: We need to / we are going to assume sync'd state between clients
+// TODO: Please batch the state updates to avoid calling firestore so many times
 export default function GamePage(props) {
-    let [allNames, setAllNames] = useState([])
-    // games collection, {room_code} document, setup collection, {name} document
-    // Ignore the promise
-    setDoc(doc(db, "games", props.room_code, "setup", props.name), {
-        "name": props.name
-    })
+    // Max of 100, up to host to maintain
 
-    const names_q = query(collection(db, "games", props.room_code, "setup"))
-    const names = []
-    onSnapshot(names_q, (query)=>query.forEach((doc)=> {
-            names.push(doc.data()["name"])
-            if(query.size === names.length && names.length !== allNames.length) {
-                setAllNames(names)
+    // Start out with some (20) initial letters, selected by the host
+    let [gridItems, setGridItems] = useState([])
+    let [handledIdx, setHandledIdx] = useState([])
+
+    function set_initial_letters(new_letters) {
+        let idx=0;
+        new_letters.forEach((letter)=>
+            {
+                setDoc(doc(db, "games", props.roomID, "letters", String(idx)), {
+                    "letter": letter
+                })
+                idx += 1
             }
-        }
-    ))
-
-    const start_q = query(collection(db, "games", props.room_code, "START_GAME"))
-    onSnapshot(start_q, (query)=>query.forEach((doc) => console.log("Game started")))
-
-    function start_game() {
-        // Send a start game message
-        setDoc(doc(db, "games", props.room_code, "START_GAME", "dummy_lets_start"), {
-            "lets_start": "lets_start"
-        })
+        )
     }
 
 
+    function handle_new_letters(doc, local_handled) {
+        // Could be out of order!
+        if(!handledIdx.includes(parseInt(doc.id)) && !local_handled.includes(parseInt(doc.id))) {
+            console.log("Set called!")
+            console.log(parseInt(doc.id))
+            setGridItems([...gridItems, doc.data()["letter"]])
+            setHandledIdx([...handledIdx, parseInt(doc.id)])
+            local_handled.push(parseInt(doc.id))
+        }
+    }
+
+    function add_new_letter_host() {
+        const letter = LETTERS[Math.floor(Math.random()*LETTERS.length)]
+        console.log(handledIdx)
+        console.log(Math.max(...handledIdx))
+        setDoc(doc(db, "games", props.roomID, "letters", String(Math.max(...handledIdx))), {
+            "letter": letter
+        })
+    }
+
+    if(gridItems.length===0 && props.host) {
+        let grid_items = []
+        for(let i=0; i<20; i++) {
+            grid_items.push(LETTERS[Math.floor(Math.random()*LETTERS.length)])
+        }
+        set_initial_letters(grid_items)
+        // setInterval(add_new_letter_host, 3000)
+    }
+
+
+    const letters_query = query(collection(db, "games", props.roomID, "letters"))
+
+    let local_idx_handled = []
+    console.log("before")
+    console.log(handledIdx)
+    unstable_batchedUpdates(() => {
+            onSnapshot(letters_query, (query) => query.forEach((doc) => {
+                handle_new_letters(doc, local_idx_handled)
+            }))
+        }
+    )
+    console.log("after")
+    console.log(handledIdx)
+
+
+    // If host, publish letters to collection "letters" with timestamp
+    // Clients keep track of timestamps they have consumed up to so that they don't consume same letter twice
+
+    // Clients publish which tokens they have used and timestamp
+    // Score when client successfully consumes characters
     return (
-      <div className={"game-page"}>
-          {props.host ? <h2>You are the host!</h2> : <div />}
-          <h3>
-                Room Code - {props.room_code}
-          </h3>
-          <h2>Players</h2>
-          {allNames.map((name)=><h3>{name}</h3>)}
-          {props.host ? <Button variant="outlined" onClick={start_game}>Start Game</Button> : <div />}
-      </div>
+        <div className={"name-page-entry"}>
+            <h1>{props.roomID}</h1>
+            <Grid container spacing={2}>
+                {gridItems.map((item)=>
+                    <Grid item xs={1}>
+                        <Button variant={"outlined"} >{item}</Button>
+                    </Grid>
+                )}
+            </Grid>
+        </div>
     );
 }
---
+++ src/RoomCreationPage.jsx
@@ -0,0 +1,34 @@
+import {Button, TextField} from "@mui/material";
+import {Pages} from "./word-game-firestore";
+import {unstable_batchedUpdates} from "react-dom";
+
+const ROOM_POSSIBLE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+export default function RoomCreationPage(props) {
+    function enter_room() {
+        props.setPage(Pages.room_waiting)
+    }
+
+    function create_room() {
+        let room_id = ""
+        for(let i=0; i < 6; i++) {
+            room_id += ROOM_POSSIBLE_CHARS[Math.floor(Math.random()*ROOM_POSSIBLE_CHARS.length)]
+        }
+        unstable_batchedUpdates(() => {
+            props.setHost(true)
+            props.setRoomID(room_id)
+            props.setPage(Pages.room_waiting)
+        })
+
+    }
+    return (
+        <div className={"name-page-entry"}>
+            <div className={"room-elements"}>Hello {props.name}!</div>
+            <div className={"room-elements"}><Button onClick={create_room}>Create a Room</Button></div>
+            <div className={"room-elements"}>
+                <TextField label={"Room ID"} onChange={(e)=>props.setRoomID(e.target.value)}/>
+                <Button onClick={enter_room}>Enter a Room</Button>
+            </div>
+        </div>
+    );
+}
